8046197e7c93960a8bc50e90ec904fc0ce9d82fd
gnasln <lnsangprod24@gmail.com>
2024-11-14 10:31:56 +0700

    add id for userdto

--- Base_BE.Application/Dtos/UserDto.cs
+++ Base_BE.Application/Dtos/UserDto.cs
@@ -2,6 +2,7 @@
 
 public class UserDto
 {
+    public string? Id { get; set; }
     public string? Fullname { get; set; }
     public string? UserName { get; set; }
     public string? Email { get; set; }
